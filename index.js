3a12fe1767b53e875ce7ca94c6c57b8203326f53
Ashutosh Vishwakarma <ashvish183@gmail.com>
2021-10-09 22:38:44 +0530

    fix

--- index.js
+++ index.js
@@ -14,25 +14,25 @@ document.addEventListener("keydown",function(event){
 function makesound(key){
     switch(key)
     {
-        case "w":var audio=new Audio("/sounds/tom-1.mp3")
+        case "w":var audio=new Audio("sounds/tom-1.mp3")
                  audio.play();
                  break;
-        case "a":var audio=new Audio("/sounds/tom-2.mp3")
+        case "a":var audio=new Audio("sounds/tom-2.mp3")
                  audio.play();
                  break;
-        case "s":var audio=new Audio("/sounds/tom-3.mp3")
+        case "s":var audio=new Audio("sounds/tom-3.mp3")
                  audio.play();
                  break;
-        case "d":var audio=new Audio("/sounds/tom-4.mp3")
+        case "d":var audio=new Audio("sounds/tom-4.mp3")
                  audio.play();
                  break;
-        case "j":var audio=new Audio("/sounds/tom-5.mp3")
+        case "j":var audio=new Audio("sounds/tom-5.mp3")
                  audio.play();
                  break;
-        case "k":var audio=new Audio("/sounds/tom-6.mp3")
+        case "k":var audio=new Audio("sounds/tom-6.mp3")
                  audio.play();
                  break;
-        case "l":var audio=new Audio("/sounds/tom-7.mp3")
+        case "l":var audio=new Audio("sounds/tom-7.mp3")
                  audio.play();
                  break;
     }
